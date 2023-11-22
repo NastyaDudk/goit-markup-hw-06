28959e935e8cc0b7ffc8d184d8c4ddbde4c56c6a
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2023-11-22 15:33:27 +0400

    newmodalhere

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ transition: opacity 300ms ease-in-out;
 const closeModal = event => {
     const target = event.target;
 
-    var span = document.getElementsByClassName("close-but")[0];
+    var span = document.getElementsByClassName("close-but-head")[0];
 
 
     setTimeout(() => {
@@ -30,6 +30,49 @@ const openModal = () => {
 
 buttonElem.addEventListener('click', openModal);
 modalElem.addEventListener('click', closeModal);
+
+
+    
+
+
+
+
+
+
+
+
+const button = document.querySelector('.hero-button');
+const modal = document.querySelector('.info-container');
+
+modal.style.cssText = `
+display: flex;
+visibility: hidden;
+opacity: 0;
+transition: opacity 300ms ease-in-out;
+`;
+
+
+const close = event => {
+    const target = event.target;
+
+    var span = document.getElementsByClassName("close-but")[0];
+
+
+    setTimeout(() => {
+        modal.style.visibility = 'hidden';
+    }, 300)
+ }
+
+
+
+
+const open = () => {
+    modal.style.visibility = 'visible';
+    modal.style.opacity = 1;
+}
+
+button.addEventListener('click', open);
+modal.addEventListener('click', close);
 
 
     
