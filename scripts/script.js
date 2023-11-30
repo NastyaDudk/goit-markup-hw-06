733639e6ac385cbcf17f8107a697b558be18222f
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2023-11-30 14:56:35 +0400

    ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-const buttonElem = document.querySelector('.gamburger-open');
-const modalElem = document.querySelector('.modal-header');
+const buttonElem = document.querySelector(".gamburger-open");
+const modalElem = document.querySelector(".modal-header");
 
 modalElem.style.cssText = `
 display: flex;
@@ -8,79 +8,58 @@ opacity: 0;
 transition: opacity 300ms ease-in-out;
 `;
 
+const closeModal = (event) => {
+  const target = event.target;
 
-const closeModal = event => {
-    const target = event.target;
+  var span = document.getElementsByClassName("close-but-head")[0];
 
-    var span = document.getElementsByClassName("close-but-head")[0];
-
-
-    setTimeout(() => {
-        modalElem.style.visibility = 'hidden';
-    }, 300)
- }
-
-
-
+  setTimeout(() => {
+    modalElem.style.visibility = "hidden";
+  }, 300);
+};
 
 const openModal = () => {
-    modalElem.style.visibility = 'visible';
-    modalElem.style.opacity = 1;
-}
+  modalElem.style.visibility = "visible";
+  modalElem.style.opacity = 1;
+};
 
-buttonElem.addEventListener('click', openModal);
-modalElem.addEventListener('click', closeModal);
+buttonElem.addEventListener("click", openModal);
+modalElem.addEventListener("click", closeModal);
 
+$(function () {
+  $(".hero-button").click(function () {
+    $(".info-container").addClass("is-open");
+  });
 
-    
+  $("#close-btn").click(function () {
+    $(".info-container").removeClass("is-open");
+  });
+});
 
+// const button = document.querySelector('.hero-button');
+// const modal = document.querySelector('.info-container');
 
+// modal.style.cssText = `;
+// display: flex;
+// visibility: hidden;
+// opacity: 0;
+// transition: opacity 300ms ease-in-out;
+// `;
 
+// const close = event => {
+//     const target = event.target;
 
+//     var span = document.getElementsByClassName("close-but")[0];
 
+//     setTimeout(() => {
+//         modal.style.visibility = 'hidden';
+//     }, 300)
+//  }
 
+// const open = () => {
+//     modal.style.visibility = 'visible';
+//     modal.style.opacity = 1;
+// }
 
-
-const button = document.querySelector('.hero-button');
-const modal = document.querySelector('.info-container');
-
-modal.style.cssText = `
-display: flex;
-visibility: hidden;
-opacity: 0;
-transition: opacity 300ms ease-in-out;
-`;
-
-
-const close = event => {
-    const target = event.target;
-
-    var span = document.getElementsByClassName("close-but")[0];
-
-
-    setTimeout(() => {
-        modal.style.visibility = 'hidden';
-    }, 300)
- }
-
-
-
-
-const open = () => {
-    modal.style.visibility = 'visible';
-    modal.style.opacity = 1;
-}
-
-button.addEventListener('click', open);
-modal.addEventListener('click', close);
-
-
-    
-
-
-
-
-
-
-
-
+// button.addEventListener('click', open);
+// modal.addEventListener('click', close);
